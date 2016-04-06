e6445a7d20a96a5076c4170b42bb928237cd0c0a
Curtis Brazzell <curtbraz@gmail.com>
2016-04-05 21:34:57 -0400

    Update MySQLSchema.sql
    Got rid of the "LastNotified" column since it's use has been depreciated.

--- MySQLSchema.sql
+++ MySQLSchema.sql
@@ -28,7 +28,6 @@ CREATE TABLE `assets` (
   `Notify` tinyint(1) NOT NULL DEFAULT '0',
   `FirstSeen` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `LastSeen` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `LastNotified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `TimesSeen` int(11) NOT NULL DEFAULT '1',
   `MinutesSince` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
