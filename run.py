db00ae5839beac80b1f7955e63dfc6d93cc9b5df
Curtis Brazzell <curtbraz@gmail.com>
2017-12-11 19:40:41 -0500

    Update run.py
    Added Support for Python 3

--- run.py
+++ run.py
@@ -1,31 +1,34 @@
 #Server Connection to MySQL:
 
-import MySQLdb
-conn = MySQLdb.connect(host= "localhost",
+import pymysql.cursors
+
+conn = pymysql.connect(host= "localhost",
                   user="root",
                   passwd="PASSWORD_GOES_HERE",
                   db="WhosHere")
 x = conn.cursor()
 
 
-## SETS UP YOUR WLAN FOR MONITOR MODE.  YOU MAY NEED TO CHANGE TO THE OUTPUT OF "iw list" FOR YOUR DEVICE THAT SUPPORTS MONITOR MODE!! 
+## SETS UP YOUR WLAN FOR MONITOR MODE.  YOU MAY NEED TO CHANGE TO THE OUTPUT OF "iw list" FOR YOUR DEVICE THAT SUPPORTS MONITOR MODE!!
 
 import subprocess
-MonMode1 = subprocess.Popen(['sudo','iw','phy','PHY_DEVICE_HERE','interface','add','mon0','type','monitor'],stdout=subprocess.PIPE)
+MonMode1 = subprocess.Popen(['sudo','iw','phy','phy1','interface','add','mon0','type','monitor'],stdout=subprocess.PIPE)
 MonMode2 = subprocess.Popen(['sudo','ifconfig','mon0','up'],stdout=subprocess.PIPE)
 
 ## CALLS TSHARK AND FILTERS FOR PROBE REQUESTS
+## /usr/bin/tshark -i mon0 -Y 'wlan.fc.type_subtype eq 4' -l
 
-proc = subprocess.Popen(['/usr/bin/tshark','-i','mon0','-Y','wlan.fc.type_subtype eq 4','-f','type mgt','-l'],stdout=subprocess.PIPE)
+proc = subprocess.Popen(['/usr/bin/tshark','-i','mon0','-Y','wlan.fc.type_subtype eq 4','-l'],stdout=subprocess.PIPE)
 #while True:
 #  line = proc.stdout.readline()
 for line in iter(proc.stdout.readline, ""):
+
   if "Probe Request" in line:
    if "da:a1:19" not in line:
-    linepostart=line.index(".") + 8
-    lineposend=line.index(" ->")
+    linepostart=line.index(".") + 10
+    lineposend=line.index(" \xe2")
     MAC=line[linepostart:lineposend]
-#    print MAC
+    print MAC
 
 ## DUMPS MAC ADDRESSES FOR DEVICES INTO MySQL TABLE IN REAL-TIME VIA STDOUT
 
@@ -36,4 +39,3 @@ for line in iter(proc.stdout.readline, ""):
        conn.rollback()
 
 conn.close()
-
