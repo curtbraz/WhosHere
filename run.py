42521c84eede368f4e71f220a062e1d5e2323e09
Curtis Brazzell <curtbraz@gmail.com>
2017-12-12 16:58:02 -0500

    Update run.py

--- run.py
+++ run.py
@@ -19,28 +19,35 @@ MonMode2 = subprocess.Popen(['sudo','ifconfig','mon0','up'],stdout=subprocess.PI
 ## CALLS TSHARK AND FILTERS FOR PROBE REQUESTS
 ## /usr/bin/tshark -i mon0 -Y 'wlan.fc.type_subtype eq 4' -l
 ## /usr/bin/tshark -l -i mon0 -Y 'wlan.fc.type_subtype eq 4' -T fields -e wlan.sa -e radiotap.dbm_antsignal -e wlan_mgt.ssid
-proc = subprocess.Popen(['/usr/bin/tshark','-l','-i','mon0','-Y','wlan.fc.type_subtype eq 4','-T','fields','-e','wlan.sa','-e','radiotap.dbm_antsignal','-e','wlan_mgt.ssid'],stdout=subprocess.PIPE)
+proc = subprocess.Popen(['/usr/bin/tshark','-l','-i','mon0','-Y','wlan.fc.type_subtype eq 4','-T','fields','-e','wlan.sa_resolved','-e','radiotap.dbm_antsignal','-e','wlan_mgt.ssid'],stdout=subprocess.PIPE)
 #proc = subprocess.Popen(['/usr/bin/tshark','-i','mon0','-Y','wlan.fc.type_subtype eq 4','-l'],stdout=subprocess.PIPE)
 #while True:
 #  line = proc.stdout.readline()
 for line in iter(proc.stdout.readline, ""):
 
 
+#  if "Probe Request" in line:
   if "da:a1:19" not in line:
+    ##linepostart=line.index(".") + 10
+    #lineposend=line.index("\x09")
+    #MAC=line[0:lineposend]
 
     line = line.rstrip("\n")
     values = line.split("\t")
 
     MAC=values[0]
-    AP=values[1]
-    DB=values[2]
+    AP=values[2]
+    DB=values[1]
 
-    print MAC
+    print values
+
+    #print line
+    #print MAC
 
 ## DUMPS MAC ADDRESSES FOR DEVICES INTO MySQL TABLE IN REAL-TIME VIA STDOUT
 
     try:
-       x.execute("""CALL InsertMac(%s)""",(MAC))
+       x.execute("""CALL InsertMac(%s,%s,%s)""",(MAC,DB,AP))
        conn.commit()
     except:
        conn.rollback()
