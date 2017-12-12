1ba7b25cb44c09bd1087905ca25d038e3f3963ff
Curtis Brazzell <curtbraz@gmail.com>
2017-12-12 15:00:16 -0500

    Update run.py
    Added future ability to grab signal strength and AP names

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #Server Connection to MySQL:
 
 import pymysql.cursors
+import re
 
 conn = pymysql.connect(host= "localhost",
                   user="root",
@@ -17,17 +18,23 @@ MonMode2 = subprocess.Popen(['sudo','ifconfig','mon0','up'],stdout=subprocess.PI
 
 ## CALLS TSHARK AND FILTERS FOR PROBE REQUESTS
 ## /usr/bin/tshark -i mon0 -Y 'wlan.fc.type_subtype eq 4' -l
-
-proc = subprocess.Popen(['/usr/bin/tshark','-i','mon0','-Y','wlan.fc.type_subtype eq 4','-l'],stdout=subprocess.PIPE)
+## /usr/bin/tshark -l -i mon0 -Y 'wlan.fc.type_subtype eq 4' -T fields -e wlan.sa -e radiotap.dbm_antsignal -e wlan_mgt.ssid
+proc = subprocess.Popen(['/usr/bin/tshark','-l','-i','mon0','-Y','wlan.fc.type_subtype eq 4','-T','fields','-e','wlan.sa','-e','radiotap.dbm_antsignal','-e','wlan_mgt.ssid'],stdout=subprocess.PIPE)
+#proc = subprocess.Popen(['/usr/bin/tshark','-i','mon0','-Y','wlan.fc.type_subtype eq 4','-l'],stdout=subprocess.PIPE)
 #while True:
 #  line = proc.stdout.readline()
 for line in iter(proc.stdout.readline, ""):
 
-  if "Probe Request" in line:
-   if "da:a1:19" not in line:
-    linepostart=line.index(".") + 10
-    lineposend=line.index(" \xe2")
-    MAC=line[linepostart:lineposend]
+
+  if "da:a1:19" not in line:
+
+    line = line.rstrip("\n")
+    values = line.split("\t")
+
+    MAC=values[0]
+    AP=values[1]
+    DB=values[2]
+
     print MAC
 
 ## DUMPS MAC ADDRESSES FOR DEVICES INTO MySQL TABLE IN REAL-TIME VIA STDOUT
