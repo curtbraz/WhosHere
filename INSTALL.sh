dd6964c77fe727685df150096ed689d0704f6e8f
Curtis Brazzell <curtbraz@gmail.com>
2018-09-07 16:21:10 -0400

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -24,9 +24,9 @@ echo "Installing Required Packages.. You Will Have to Create a Database Password
 
 sleep 2;
 
-sudo apt-get install apache2 php mysql-client mysql-server php-mysqlnd python-mysqldb tshark mysql-server python3-pymysql -y;
+sudo apt-get install apache2 php mysql-client mysql-server php-mysqlnd python-mysqldb tshark mysql-server python-pip python3-pymysql -y;
 
-
+pip install pymysql;
 
 ## SETS UP MONITORING WLAN INTERFACE
 
