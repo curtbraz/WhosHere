ad6597eca6c3375de4223cd6627b9d1e0db198e2
Curtis Brazzell <curtbraz@gmail.com>
2018-09-07 15:53:10 -0400

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -3,7 +3,7 @@
 // UPDATE THESE TO REFLECT YOUR MySQL INSTANCE FROM INSTALLATION
 
 $servername = "127.0.0.1";
-$username = "root";
+$username = "whoshere";
 $password = "PASSWORD_GOES_HERE";
 $dbname = "WhosHere";
 
